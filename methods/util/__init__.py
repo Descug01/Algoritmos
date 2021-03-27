861f2aab570b9558b5de864dcf6f7e7adb5714b7
Descug01 <descug01@estudiantes.unileon.es>
2021-03-27 13:04:58 +0100

    Add files via upload

---
+++ methods/util/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+#from .data import (
+#        binarize_labels, split_data_label,
+#        structured_split, normalize, standarize, train_test_split,
+#        random_extract, split_gen
+#)
+#from .graph import nearest_same_class_neighbors, adjacency_matrix, laplacian
+#from .matrix import (
+#        simmetrize, construct_L, l21_norm, top_eigenvectors, centering_matrix
+#)
+
+#from . import data
+#from . import graph
+#from . import matrix
